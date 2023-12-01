5e18c611a3b474269c50d51b1f3ee2ea73bc30ee
SimoneMattioli98 <s.mattioli@eye-track.it>
2023-12-01 15:04:10 +0100

    Revert "this is a test commit"
    This reverts commit 66cf80ba0efdeedc77778beab5183bf453ff95f6.

--- mmdet/apis/inference.py
+++ mmdet/apis/inference.py
@@ -49,7 +49,6 @@ def init_detector(
     Returns:
         nn.Module: The constructed detector.
     """
-    print("THIS IS A TEST")
     if isinstance(config, (str, Path)):
         config = Config.fromfile(config)
     elif not isinstance(config, Config):
